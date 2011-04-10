d31dd110c71f342ce51d7ba045ebffca4190cb96
David Henner <drhenner@yahoo.com>
2011-04-10 00:20:18 -0700

    welcome depends on the cookie set

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,15 +3,19 @@ class WelcomeController < ApplicationController
   layout 'welcome'
 
   def index
-    @featured_product = Product.featured# FeaturedItem.now
-    #@best_selling_products = Product.limit(5)
-    #@other_products  ## search 2 or 3 categories (maybe based on the user)
-    unless @featured_product
-      if current_user && current_user.admin?
-        redirect_to admin_merchandise_products_url
-      else
-        redirect_to login_url
-      end
+    cookies[:trade_show]  = params[:trade_show] if params[:trade_show].present?
+    cookies[:location]    = params[:location]   if params[:location].present?
+
+    if cookies[:trade_show] # Display the trade show
+      @trade_show = TradeShow.where(["trade_shows.id = ?", cookies[:trade_show] ])
+      render :template => '/welcome/trade_show'
+    elsif cookies[:location] # Display all trade shows at this location
+      @trade_shows = TradeShow.where(["trade_shows.city_id = ? AND active = ?", cookies[:location], true])
+      render :template => '/welcome/trade_shows'
+    else # Display all locations
+      @cities = City.order('cities.name DESC')
+      render :template => '/welcome/locations'
     end
+
   end
 end
